c7f60687a935c0f67213bb93c51c4913cb6d9ea9
dejanpan <dianalvesilva12@gmail.com>
2020-03-28 15:06:36 -0700

    new table in sqlite and app.py running

--- app.py
+++ app.py
@@ -17,10 +17,8 @@ disk_engine = create_engine('sqlite:///olympic_events.sqlite')
 Base = automap_base()
 Base.prepare(disk_engine, reflect=True)
 
-# Events wasn't working for me, so I commented it out for now (HY)
-# Events = Base.classes.events
-Olympians_Team_Final = Base.classes.olympians_team_final
-Medals_Team_Total = Base.classes.medals_team_total
+Events = Base.classes.events
+Events_Final = Base.classes.events_final
 
 @app.route("/")
 def index():
@@ -28,33 +26,126 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/olympians_team/<NOC>")
-def olympians_team(NOC):
+@app.route("/events_final/<NOC>")
+def events_final(NOC):
 
     session = Session(disk_engine)
-    
-    results = session.query(Olympians_Team_Final.Year, Olympians_Team_Final.Season,\
-                        Olympians_Team_Final.Team, Olympians_Team_Final.NOC, Olympians_Team_Final.No_olympians).filter(Olympians_Team_Final.NOC == NOC).all()
+
+    sel = [
+        Events_Final.Year,
+        Events_Final.Season,
+        Events_Final.City,
+        Events_Final.NOC,
+        Events_Final.Country,
+        Events_Final.Game_Label,
+        Events_Final.GDP,
+        Events_Final.Population,
+        Events_Final.No_olympians,
+        Events_Final.Bronze_athlete,
+        Events_Final.Gold_athlete,
+        Events_Final.Silver_athlete,
+        Events_Final.Total_Medals_athlete,
+        Events_Final.No_olympians,
+        Events_Final.Bronze_team,
+        Events_Final.Gold_team,
+        Events_Final.Silver_team,
+        Events_Final.Total_Medals_team
+        ]
+
+    results = session.query(*sel).filter(Events_Final.NOC == NOC).all()
 
     session.close ()
 
     # Create a dictionary entry for each row of Combined dataframe
-    olympians_team_data = []
+    events_final_data = []
     for result in results:
         combined = {}
         combined["Year"] = result[0]
         combined["Season"] = result[1]
-        combined["Team"] = result[2]
+        combined["City"] = result[2]
         combined["NOC"] = result[3]
-        combined["No_olympians"] = result[4]
-        olympians_team_data.append(combined)
+        combined["Country"] = result[4]
+        combined["Game_Label"] = result[5]
+        combined["GDP"] = result[6]
+        combined["Population"] = result[7]
+        combined["No_olympians"] = result[8]
+        combined["Bronze_athlete"] = result[9]
+        combined["Gold_athlete"] = result[10]
+        combined["Silver_athlete"] = result[11]
+        combined["Total_Medals_athlete"] = result[12]
+        combined["Bronze_team"] = result[13]
+        combined["Gold_team"] = result[14]
+        combined["Silver_team"] = result[15]
+        combined["Total_Medals_team"] = result[16]
+
+        events_final_data.append(combined)
         
 
     # print(olympians_team_data)
    
-    return jsonify(olympians_team_data)
+    return jsonify(events_final_data)
 
+
+@app.route("/events_final/")
+def events_final2():
+
+    session = Session(disk_engine)
+
+    sel = [
+        Events_Final.Year,
+        Events_Final.Season,
+        Events_Final.City,
+        Events_Final.NOC,
+        Events_Final.Country,
+        Events_Final.Game_Label,
+        Events_Final.GDP,
+        Events_Final.Population,
+        Events_Final.No_olympians,
+        Events_Final.Bronze_athlete,
+        Events_Final.Gold_athlete,
+        Events_Final.Silver_athlete,
+        Events_Final.Total_Medals_athlete,
+        Events_Final.No_olympians,
+        Events_Final.Bronze_team,
+        Events_Final.Gold_team,
+        Events_Final.Silver_team,
+        Events_Final.Total_Medals_team
+        ]
+
+    results = session.query(*sel).all()
+
+    session.close ()
+
+    # Create a dictionary entry for each row of Combined dataframe
+    events_final_data = []
+    for result in results:
+        combined = {}
+        combined["Year"] = result[0]
+        combined["Season"] = result[1]
+        combined["City"] = result[2]
+        combined["NOC"] = result[3]
+        combined["Country"] = result[4]
+        combined["Game_Label"] = result[5]
+        combined["GDP"] = result[6]
+        combined["Population"] = result[7]
+        combined["No_olympians"] = result[8]
+        combined["Bronze_athlete"] = result[9]
+        combined["Gold_athlete"] = result[10]
+        combined["Silver_athlete"] = result[11]
+        combined["Total_Medals_athlete"] = result[12]
+        combined["Bronze_team"] = result[13]
+        combined["Gold_team"] = result[14]
+        combined["Silver_team"] = result[15]
+        combined["Total_Medals_team"] = result[16]
+
+        events_final_data.append(combined)
+        
+
+    # print(olympians_team_data)
+   
+    return jsonify(events_final_data)
 
 
 if __name__ == "__main__":
     app.run(debug=True)
+
