c69ec02ee201a44b25581aa43c38ab13c2825132
dejanpan <dianalvesilva12@gmail.com>
2020-03-28 10:15:32 -0700

    Flask App with cleaner code

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func, inspect, Column, Integer, String, Float
 from sqlalchemy.ext.declarative import declarative_base
-Base = declarative_base()
 
 from flask import Flask, jsonify, render_template
 app = Flask(__name__)
@@ -17,8 +16,6 @@ app = Flask(__name__)
 disk_engine = create_engine('sqlite:///olympic_events.sqlite')
 Base = automap_base()
 Base.prepare(disk_engine, reflect=True)
-# open and close session in each of the routes. Delete code below (HY).
-# session = Session(disk_engine) 
 
 # Events wasn't working for me, so I commented it out for now (HY)
 # Events = Base.classes.events
@@ -33,13 +30,12 @@ def index():
 
 @app.route("/olympians_team/<NOC>")
 def olympians_team(NOC):
-    # HY added this - you have to open and close sessions in each route
+
     session = Session(disk_engine)
     
     results = session.query(Olympians_Team_Final.Year, Olympians_Team_Final.Season,\
                         Olympians_Team_Final.Team, Olympians_Team_Final.NOC, Olympians_Team_Final.No_olympians).filter(Olympians_Team_Final.NOC == NOC).all()
 
-    # HY added this - you have to open and close sessions in each route
     session.close ()
 
     # Create a dictionary entry for each row of Combined dataframe
@@ -53,7 +49,8 @@ def olympians_team(NOC):
         combined["No_olympians"] = result[4]
         olympians_team_data.append(combined)
         
-    print(olympians_team_data)
+
+    # print(olympians_team_data)
    
     return jsonify(olympians_team_data)
 
