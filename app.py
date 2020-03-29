dd96c2907499ca2e46a64949185518c21df4054c
dejanpan <dianalvesilva12@gmail.com>
2020-03-29 01:46:00 -0700

    updated Flask app with last requirements

--- app.py
+++ app.py
@@ -25,9 +25,65 @@ def index():
     """Return the homepage."""
     return render_template("index.html")
 
+@app.route("/events_final_country/")
+def events_final_country():
 
-@app.route("/events_final/<NOC>")
-def events_final(NOC):
+    session = Session(disk_engine)
+
+    sel = [
+        Events_Final.Year,
+        Events_Final.Season,
+        Events_Final.City,
+        Events_Final.NOC,
+        Events_Final.Country,
+        Events_Final.Game_Label,
+        Events_Final.GDP,
+        Events_Final.Population,
+        Events_Final.No_olympians,
+        Events_Final.Bronze_athlete,
+        Events_Final.Gold_athlete,
+        Events_Final.Silver_athlete,
+        Events_Final.Total_Medals_athlete,
+        Events_Final.No_olympians,
+        Events_Final.Bronze_team,
+        Events_Final.Gold_team,
+        Events_Final.Silver_team,
+        Events_Final.Total_Medals_team
+        ]
+
+    results = session.query(*sel).all()
+
+    session.close ()
+
+    # Create a dictionary entry for each row of Combined dataframe
+    events_final_data = []
+    for result in results:
+        combined = {}
+        combined["Year"] = result[0]
+        combined["Season"] = result[1]
+        combined["City"] = result[2]
+        combined["NOC"] = result[3]
+        combined["Country"] = result[4]
+        combined["Game_Label"] = result[5]
+        combined["GDP"] = result[6]
+        combined["Population"] = result[7]
+        combined["No_olympians"] = result[8]
+        combined["Bronze_athlete"] = result[9]
+        combined["Gold_athlete"] = result[10]
+        combined["Silver_athlete"] = result[11]
+        combined["Total_Medals_athlete"] = result[12]
+        combined["Bronze_team"] = result[13]
+        combined["Gold_team"] = result[14]
+        combined["Silver_team"] = result[15]
+        combined["Total_Medals_team"] = result[16]
+
+        events_final_data.append(combined)
+   
+    return jsonify(events_final_data)
+
+
+@app.route("/events_final_country/<NOC>")
+def events_final_country_noc(NOC):
 
     session = Session(disk_engine)
 
@@ -80,14 +136,11 @@ def events_final(NOC):
 
         events_final_data.append(combined)
         
-
-    # print(olympians_team_data)
-   
     return jsonify(events_final_data)
 
 
-@app.route("/events_final/")
-def events_final2():
+@app.route("/events_final_games/<Game_Label>")
+def events_final_games(Game_Label):
 
     session = Session(disk_engine)
 
@@ -112,7 +165,7 @@ def events_final2():
         Events_Final.Total_Medals_team
         ]
 
-    results = session.query(*sel).all()
+    results = session.query(*sel).filter(Events_Final.Game_Label.like(f"%{Game_Label}%")).all()
 
     session.close ()
 
@@ -139,12 +192,8 @@ def events_final2():
         combined["Total_Medals_team"] = result[16]
 
         events_final_data.append(combined)
-        
-
-    # print(olympians_team_data)
    
     return jsonify(events_final_data)
-
 
 if __name__ == "__main__":
     app.run(debug=True)
