0e47cb39e8c2f8b78c25ea456297292dae64de8b
dejanpan <dianalvesilva12@gmail.com>
2020-04-01 02:41:06 -0700

    updated jupyter Notebook and app.py

--- app.py
+++ app.py
@@ -19,6 +19,7 @@ Base.prepare(disk_engine, reflect=True)
 
 Events = Base.classes.events
 Events_Final = Base.classes.events_final
+# Countries_per_Event = Base.classes.countries_per_event
 
 @app.route("/")
 def index():
@@ -45,10 +46,12 @@ def events_final_country():
         Events_Final.Season,
         Events_Final.City,
         Events_Final.Game_Label,
+        Events_Final.Chart_Label,
         Events_Final.NOC,
         Events_Final.Country,
         Events_Final.GDP,
         Events_Final.Population,
+        Events_Final.GDP_per_capita,
         Events_Final.No_olympians,
         Events_Final.Bronze_athlete,
         Events_Final.Gold_athlete,
@@ -72,19 +75,21 @@ def events_final_country():
         combined["Season"] = result[1]
         combined["City"] = result[2]
         combined["Game_Label"] = result[3]
-        combined["NOC"] = result[4]
-        combined["Country"] = result[5]
-        combined["GDP"] = result[6]
-        combined["Population"] = result[7]
-        combined["No_olympians"] = result[8]
-        combined["Bronze_athlete"] = result[9]
-        combined["Gold_athlete"] = result[10]
-        combined["Silver_athlete"] = result[11]
-        combined["Total_Medals_athlete"] = result[12]
-        combined["Bronze_team"] = result[13]
-        combined["Gold_team"] = result[14]
-        combined["Silver_team"] = result[15]
-        combined["Total_Medals_team"] = result[16]
+        combined["Chart_Label"] = result[4]
+        combined["NOC"] = result[5]
+        combined["Country"] = result[6]
+        combined["GDP"] = result[7]
+        combined["Population"] = result[8]
+        combined["GDP_per_capita"] = result[9]
+        combined["No_olympians"] = result[10]
+        combined["Bronze_athlete"] = result[11]
+        combined["Gold_athlete"] = result[12]
+        combined["Silver_athlete"] = result[13]
+        combined["Total_Medals_athlete"] = result[14]
+        combined["Bronze_team"] = result[15]
+        combined["Gold_team"] = result[16]
+        combined["Silver_team"] = result[17]
+        combined["Total_Medals_team"] = result[18]
 
         events_final_data.append(combined)
    
@@ -101,10 +106,12 @@ def events_final_country_noc(NOC):
         Events_Final.Season,
         Events_Final.City,
         Events_Final.Game_Label,
+        Events_Final.Chart_Label,
         Events_Final.NOC,
         Events_Final.Country,
         Events_Final.GDP,
         Events_Final.Population,
+        Events_Final.GDP_per_capita,
         Events_Final.No_olympians,
         Events_Final.Bronze_athlete,
         Events_Final.Gold_athlete,
@@ -128,19 +135,21 @@ def events_final_country_noc(NOC):
         combined["Season"] = result[1]
         combined["City"] = result[2]
         combined["Game_Label"] = result[3]
-        combined["NOC"] = result[4]
-        combined["Country"] = result[5]
-        combined["GDP"] = result[6]
-        combined["Population"] = result[7]
-        combined["No_olympians"] = result[8]
-        combined["Bronze_athlete"] = result[9]
-        combined["Gold_athlete"] = result[10]
-        combined["Silver_athlete"] = result[11]
-        combined["Total_Medals_athlete"] = result[12]
-        combined["Bronze_team"] = result[13]
-        combined["Gold_team"] = result[14]
-        combined["Silver_team"] = result[15]
-        combined["Total_Medals_team"] = result[16]
+        combined["Chart_Label"] = result[4]
+        combined["NOC"] = result[5]
+        combined["Country"] = result[6]
+        combined["GDP"] = result[7]
+        combined["Population"] = result[8]
+        combined["GDP_per_capita"] = result[9]
+        combined["No_olympians"] = result[10]
+        combined["Bronze_athlete"] = result[11]
+        combined["Gold_athlete"] = result[12]
+        combined["Silver_athlete"] = result[13]
+        combined["Total_Medals_athlete"] = result[14]
+        combined["Bronze_team"] = result[15]
+        combined["Gold_team"] = result[16]
+        combined["Silver_team"] = result[17]
+        combined["Total_Medals_team"] = result[18]
 
         events_final_data.append(combined)
         
@@ -157,10 +166,12 @@ def events_final_games(Game_Label):
         Events_Final.Season,
         Events_Final.City,
         Events_Final.Game_Label,
+        Events_Final.Chart_Label,
         Events_Final.NOC,
         Events_Final.Country,
         Events_Final.GDP,
         Events_Final.Population,
+        Events_Final.GDP_per_capita,
         Events_Final.No_olympians,
         Events_Final.Bronze_athlete,
         Events_Final.Gold_athlete,
@@ -184,19 +195,21 @@ def events_final_games(Game_Label):
         combined["Season"] = result[1]
         combined["City"] = result[2]
         combined["Game_Label"] = result[3]
-        combined["NOC"] = result[4]
-        combined["Country"] = result[5]
-        combined["GDP"] = result[6]
-        combined["Population"] = result[7]
-        combined["No_olympians"] = result[8]
-        combined["Bronze_athlete"] = result[9]
-        combined["Gold_athlete"] = result[10]
-        combined["Silver_athlete"] = result[11]
-        combined["Total_Medals_athlete"] = result[12]
-        combined["Bronze_team"] = result[13]
-        combined["Gold_team"] = result[14]
-        combined["Silver_team"] = result[15]
-        combined["Total_Medals_team"] = result[16]
+        combined["Chart_Label"] = result[4]
+        combined["NOC"] = result[5]
+        combined["Country"] = result[6]
+        combined["GDP"] = result[7]
+        combined["Population"] = result[8]
+        combined["GDP_per_capita"] = result[9]
+        combined["No_olympians"] = result[10]
+        combined["Bronze_athlete"] = result[11]
+        combined["Gold_athlete"] = result[12]
+        combined["Silver_athlete"] = result[13]
+        combined["Total_Medals_athlete"] = result[14]
+        combined["Bronze_team"] = result[15]
+        combined["Gold_team"] = result[16]
+        combined["Silver_team"] = result[17]
+        combined["Total_Medals_team"] = result[18]
 
         events_final_data.append(combined)
    
@@ -228,6 +241,39 @@ def NOC_Country_list():
         NOC_country_list.append(combined)
    
     return jsonify(NOC_country_list)
+
+@app.route("/countries_per_event/")
+def countries_per_event():
+
+    session = Session(disk_engine)
+
+    sel = [
+        Events_Final.Year,
+        Events_Final.Season,
+        Events_Final.City,
+        Events_Final.Game_Label,
+        Events_Final.Chart_Label,
+        func.count(Events_Final.NOC)
+        ]
+
+    results = session.query(*sel).group_by(*sel[0:5]).all()
+
+    session.close ()
+
+    # Create a dictionary entry for each row of Combined dataframe
+    events_final_data = []
+    for result in results:
+        combined = {}
+        combined["Year"] = result[0]
+        combined["Season"] = result[1]
+        combined["City"] = result[2]
+        combined["Game_Label"] = result[3]
+        combined["Chart_Label"] = result[4]
+        combined["Total_Countries"] = result[5]
+
+        events_final_data.append(combined)
+   
+    return jsonify(events_final_data)
 
 if __name__ == "__main__":
     app.run(debug=True)
