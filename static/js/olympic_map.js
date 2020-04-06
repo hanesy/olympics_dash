bb66367da1658ae8ad029214e391fa3e9afa0781
Shaymus McTeague <Shaymus.McTeague@arm.com>
2020-04-05 17:26:01 -0700

    update map js to remove unused lines

--- static/js/olympic_map.js
+++ static/js/olympic_map.js
@@ -4,16 +4,12 @@ let ajax_progress;
 let current_country = '';
 
 let filter_year = "";
-// let filter_season = "";
 
 // Create function for year change
 function changeYear(element) {
     filter_year = element.value;
 }
-// Create function for season (winter or summer)
-// function changeSeason(element) {
-//     filter_season = element.value;
-// }
+
 
 $(document).ready(function(){
     console.log(medal_data);
@@ -33,7 +29,7 @@ $(document).ready(function(){
     let latlng = e.latlng;
     if (!ajax_progress) {
         ajax_progress = $.ajax({
-            url: 'https://maps.googleapis.com/maps/api/geocode/json?latlng=' + e.latlng.lat + ',' + e.latlng.lng + '&sensor=true&key=AIzaSyCVsbH_wdEgmvtsD_-9Ai0IhrA54vC0Bzw',
+            url: 'https://maps.googleapis.com/maps/api/geocode/json?latlng=' + e.latlng.lat + ',' + e.latlng.lng + '&sensor=true&key=AIzaSyDIahGO-5SZ9yDAgJsEwTMB_pwzzfGnK4k',
             type: "GET",
             cache: false,
             beforeSend: function () {
@@ -78,7 +74,6 @@ $(document).ready(function(){
 
                     let bronze = 0;
                     let gold = 0;
-                    // let no_medal = 0;
                     let silver = 0;
                     let total = 0;
 
@@ -90,17 +85,15 @@ $(document).ready(function(){
 
                         bronze += medal_data.Bronze_team[key];
                         gold += medal_data.Gold_team[key];
-                        // no_medal += medal_data.No_medal[key];
                         silver += medal_data.Silver_team[key];
                         total += medal_data.Total_Medals_team[key];
                     }
                     }
-
+                    // Popup content
                     popup.setContent('<strong style="font-size:14px;">' + current_country +
                                      '</strong><br><strong>Gold Medal: </strong>' + gold + 
                                      '<br><strong>Silver Medal: </strong>' + silver + 
                                      '<br><strong>Bronze Medal: </strong>' + bronze + 
-                                    //  '<br><strong>No Medal: </strong>' + no_medal +
                                      '<br><strong>Total Medals: </strong>' + total);
                 }
                 }
@@ -118,6 +111,7 @@ $(document).ready(function(){
     })
     .setView([42.35, -71.08], 3);
 
+    // Print Map Layer
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
         maxZoom: 17,
